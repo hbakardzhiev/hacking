2c43ff94fc37f22294eea085d1ce6b253ab70662
Ne184 <64256244+Ne184@users.noreply.github.com>
2022-07-04 23:04:58 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,11 +1,11 @@
 # Flask server that responds on M1 requesting content after being DNSspoofed
 from flask import Flask
-app = Flask(__name__, static_url_path = "", static_folder="")
+app = Flask(__name__, static_url_path = "", static_folder="")# name is the file name, static_url_path/folder show where is the google.html
 
 
 @app.route("/")
 def hello():
-    return app.send_static_file("google.html")
+    return app.send_static_file("google.html")# google.html can be evrything
 
 # flask run -h localhost -p 8081
 if __name__ == "__main__":
